a69dabb8a9fad0abe36486199dd41c1d40fae8ee
PSI <tkddls8848@gmail.com>
2021-08-28 20:34:36 +0900

    Auth Navigation part 2

--- navigation/Auth.js
+++ navigation/Auth.js
@@ -3,11 +3,22 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Welcome from "../screens/Welcome";
 import SignIn from "../screens/SignIn";
 import SignUp from "../screens/SignUp";
+import { Ionicons } from "@expo/vector-icons";
+import { Platform } from "react-native";
 
 const Auth = createStackNavigator();
 
+const IsAndroid = Platform.OS
+
 export default () => (
-  <Auth.Navigator>
+  <Auth.Navigator mode="float"
+  screenOptions={{
+      headerBackTitleVisible: false,
+      headerTransparent: true,
+      headerBackImage: () =>(
+            <Ionicons name={IsAndroid=="android" ? "md-home" : "ios-arrow-back"} size={24} color="black" />
+      )
+  }}>
     <Auth.Screen name="Welcome" component={Welcome} />
     <Auth.Screen name="SignIn" component={SignIn} />
     <Auth.Screen name="SignUp" component={SignUp} />
--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+import SignIn from "../screens/SignIn"
 
-export default () => (
-    <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
-        <Text>
-            Welcome
-        </Text>
-    </View>
-);
+
+export default ({navigation}) => {
+        console.log(navigation);
+        return(
+            <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+            <Text>
+                Welcome
+            </Text>
+            <Button onPress={() => navigation.navigate("SignIn") } title={"Sign In"}/>
+            <Button onPress={() => navigation.navigate("SignUp")} title={"Sign Up"}/>
+        </View>
+        )
+}
