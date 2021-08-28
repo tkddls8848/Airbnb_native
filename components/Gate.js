4019dfa9acab15f8eed6f3fcc10aa04bd99078a0
PSI <tkddls8848@gmail.com>
2021-08-28 19:03:59 +0900

    Auth Navigation part 1

--- components/Gate.js
+++ components/Gate.js
@@ -2,20 +2,20 @@ import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { useSelector , useDispatch} from "react-redux";
 import {logIn, logOut} from "../redux/userSlice";
+import Auth from "../navigation/Auth"
+import { NavigationContainer } from "@react-navigation/native";
 
 export default () => {
   const userslice = useSelector(state => state.usersReducer);
   const dispatch = useDispatch();
   return (
-    <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
-      {userslice.isLoggedIn ? 
-        <TouchableOpacity onPress={() =>dispatch(logOut())}>
-          <Text>Log Out</Text>
-        </TouchableOpacity> : 
-        <TouchableOpacity onPress={() => dispatch(logIn("bs.token"))}>
-          <Text>Log In</Text>
-        </TouchableOpacity>
-      }
-    </View>
+    <NavigationContainer>
+        {userslice.isLoggedIn ? 
+          <TouchableOpacity onPress={() =>dispatch(logOut())}>
+            <Text>Log Out</Text>
+          </TouchableOpacity> : 
+            <Auth></Auth>
+        }
+    </NavigationContainer>
   );
 };
